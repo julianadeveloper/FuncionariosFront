a4a8153b5769f80a2fef29c5d0a23dc60eeed5f2
Juliana Oliveira <juliana.oliveira@tallos.com.br>
2022-06-02 12:05:22 -0300

    install dependencides

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,5 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
-
+import '@fortawesome/fontawesome-free'
 createApp(App).use(store).use(router).mount('#app')
