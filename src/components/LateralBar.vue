e7488630a8f74fc665b7fde2a348f5c2a7364f88
Juliana Oliveira <juliana.oliveira@tallos.com.br>
2022-08-04 15:49:51 -0300

    menu dropdown

--- src/components/LateralBar.vue
+++ src/components/LateralBar.vue
@@ -1,38 +1,45 @@
 <template>
   <header class="header">
     <img id="logo" src="../assets/images/logo.svg" alt="logo-vuaida" />
-    <div class="dropdown-menu" id="dropdown-menu" role="menu">
-      <div class="dropdow-content">
-        <a
-          href="#"
-          class="dropdown-item home-icon"
-          @click="$router.push({ name: 'DashBoardView' })"
-        >
-          Home
-          <!-- <i class="fa-solid fa-house-user fa-lg"></i> -->
-        </a>
+
+    <div>
+      <UserProfile />
+      <div class="dropdown">
+        <button class="dropdown-trigger" @click="openDropDown">
+          <i class="fas fa-angle-down" aria-hidden="true"></i>
+          <span class="icon is-small"> </span>
+        </button>
+
+        <div v-if="DropDown">
+          <div class="dropdown-content">
+            <a class="icon-theme dropdown-item" @click="changeTheme">
+              <!-- <i class="fa-solid fa-moon texto-botao"></i> -->
+
+              {{ textButton }}
+            </a>
+
+            <button
+              class="home-icon dropdown-item"
+              @click="$router.push({ name: 'DashBoardView' })"
+            >
+              <!-- <i class="fa-solid fa-house-user fa-lg"></i> -->
+              Home
+            </button>
+            <button
+              class="home-icon dropdown-item"
+              @click="$router.push({ name: 'CreateView' })"
+            >
+              <!-- <i class="fa-solid fa-house-user fa-lg"></i> -->
+              Cadastrar
+            </button>
+            <button class="logout-icon dropdown-item" @click.prevent="logout">
+              Sair
+              <i class="fa-solid fa-arrow-right-from-bracket fa-lg"></i>
+            </button>
+          </div>
+        </div>
       </div>
     </div>
-
-    <a class="icon-theme" @click="changeTheme">
-      <i class="fa-solid fa-moon"></i>
-
-      {{ textButton }}
-    </a>
-
-    <button class="home-icon" @click="$router.push({ name: 'DashBoardView' })">
-      Home
-      <i class="fa-solid fa-house-user fa-lg"></i>
-    </button>
-
-    <button class="logout-icon" @click.prevent="logout">
-      Sair
-      <i class="fa-solid fa-arrow-right-from-bracket fa-lg"></i>
-
-    </button>
-          <div>
-        <UserProfile/>
-      </div>
   </header>
 </template>
 
@@ -40,63 +47,71 @@
 import { logoutService } from "@/services/logout";
 import { defineComponent } from "vue";
 import { POSITION, useToast } from "vue-toastification";
-import  UserProfile  from '../components/UserProfile.vue'
+import UserProfile from "../components/UserProfile.vue";
 export default defineComponent({
   name: "LateralBar",
-  components: { UserProfile },
   data() {
     return {
+      DropDown: false,
       DarkThemeOn: false,
       logoutService: new logoutService(this.$router),
     };
   },
   computed: {
+       
+
     textButton() {
       if (!this.DarkThemeOn) {
-        return "Dark";
+        return "Tema Dark";
       } else {
-        return "Light";
+        return "Tema Light";
       }
     },
   },
   methods: {
-    chamaToast() {
-      const toast = useToast();
-
-      // or with options
-      toast.warning("Você fez logout", {
-        position: POSITION.BOTTOM_RIGHT,
-        timeout: 2000,
-      });
+        chamaToast(){
+           const toast = useToast();
+   
+         // or with options
+         toast.warning("Você fez logout", {
+           position: POSITION.BOTTOM_RIGHT,
+           timeout: 2000
+         })
     },
-    async logout() {
-      await this.logoutService.logoutUser();
-      this.chamaToast();
+   async  logout() {
+      await this.logoutService.logoutUser()
+      this.chamaToast()
     },
     changeTheme() {
       this.DarkThemeOn = !this.DarkThemeOn;
       this.$emit("theme", this.DarkThemeOn);
+    },
+    openDropDown() {
+      this.DropDown = !this.DropDown;
+      console.log("dropdown", this.DropDown);
     },
   },
 });
 </script>
 
 <style scoped>
+.dropdown {
+  top: 30px;
+  position: absolute;
+}
 .header {
+  color: black;
   display: flex;
+  flex-direction: column;
   align-items: center;
-  justify-content: space-between;
-  width: 100vw;
-  height: 6rem;
-  padding: 1rem;
+  justify-content: center;
+  width: 10rem;
+  height: 100vh;
   /* background: rgba(17, 16, 16, 0.747); */
-  background: #ebebee;
+  background: #2a2f67;
   /* box-shadow: 4px 5px 20px; */
 }
-#logo {
-  max-width: 100px;
-  height: auto;
-}
+
 button {
   cursor: pointer;
   border: 1px solid transparent;
